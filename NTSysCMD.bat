12bcda17f430c72c3e399f3f6d121bcbd3e790bd
TheRedSteak <47194215+TheRedSteak@users.noreply.github.com>
2020-10-10 09:35:13 +0200

    Update NTSysCMD.bat

--- NTSysCMD.bat
+++ NTSysCMD.bat
@@ -34,20 +34,25 @@ mkdir C:\Temp\NTSysCMD
 echo Created Directory
 echo Starting download of files in new CMD Window
 REM  --> Start download of the archive in new CMD.. starting in current CMD does not work May fix in newer versions.
+REM  --> Any way to actually hide the CMD window?
 start cmd.exe /c "curl -A -L "https://download.sysinternals.com/files/PSTools.zip" -o C:\Temp\NTSysCMD\PSTools.zip"
 echo Waiting for download to finish
+REM  --> Waiting for curl to finish - I want to make it detect when download is finished in newer versions.
 timeout 5
 cd C:\Temp\NTSysCMD\
+REM  --> Extracts the archive
 tar -xf PSTools.zip
 echo Extracting archive
 echo Doing the magic..
+REM  --> Starts CMD under nt authority/system user
 psexec -nobanner -accepteula -sid %SYSTEMROOT%\System32\cmd.exe
 cls
 echo Magic done! Check new CMD window.
 echo Cleaning up
-REM  --> Files are being deleted and script is terminated.
+REM  --> Deletes files it downloaded and also deletes the folder
 cd C:\Temp\
 @RD /S /Q "C:\Temp\NTSysCMD"
 echo You can now exit this CMD Window by pressing any key! Have a nice day.
+REM  --> Debug timeout below.. will probably remove.
 timeout 20
 exit
